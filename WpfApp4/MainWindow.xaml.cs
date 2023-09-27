88467a0a3fb72fe6fa9a082e8fae47283cdb642f
KevinChena <z6256875@gmail.com>
2023-09-27 10:33:18 +0800

    加入專案檔案。

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Triangle> triangles = new List<Triangle>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //檢查是否為三角形
+            trianglesCheck(triangles);
+
+        }
+
+        private bool trianglesCheck(List<Triangle> mytriangles)
+        {   
+            bool answer = false;
+            foreach (var item in mytriangles)
+            {
+                if ((item.Side1) * (item.Side1)+(item.Side2) * (item.Side2) == item.Side3* item.Side3) answer = true;
+                else if ((item.Side1) * (item.Side1)+(item.Side3)*(item.Side3) == item.Side2* item.Side2) answer = true;
+                else if ((item.Side2) * (item.Side2)+(item.Side3)*(item.Side3) == item.Side1* item.Side1) answer = true;
+                else answer = false;
+
+            }
+            return answer;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            var targetTextBox = sender as TextBox;
+            bool success = double.TryParse(targetTextBox.Text, out double amount);
+
+            if (!success && !string.IsNullOrEmpty(targetTextBox.Text))
+            {
+                MessageBox.Show("請輸入數字，輸入錯誤");
+                targetTextBox.Clear();
+            }
+            else if (amount <= 0 && !string.IsNullOrEmpty(targetTextBox.Text))
+            {
+                MessageBox.Show("輸入數值必須大於0，輸入錯誤");
+                targetTextBox.Clear();
+            }
+
+
+        }
+
+        private void Botton_Click(object sender, RoutedEventArgs e)
+        {
+            triangles.Add(new Triangle()
+            {
+                Side1 = double.Parse(textbox1.Text),
+                Side2 = double.Parse(textbox2.Text),
+                Side3 = double.Parse(textbox3.Text),
+                Comfirm = false
+            }) ;
+
+
+
+
+
+            foreach (var item in triangles)
+            {
+                item.Comfirm= trianglesCheck(triangles); ; //判斷是否為三角形的函數
+                if (item.Comfirm == true)
+                {
+                    labelbar.Content = $"邊長{item.Side1},{item.Side2},{item.Side3} 可構成三角形";
+                    labelbar.Background = System.Windows.Media.Brushes.Green;
+
+                }
+                else
+                {
+                    labelbar.Content = $"邊長{item.Side1},{item.Side2},{item.Side3} 不可構成三角形";
+                    labelbar.Background = System.Windows.Media.Brushes.Red;
+                }
+                
+                
+
+
+
+            }
+        }
+    }
+}
---
+++ WpfApp4/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class Triangle
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        public bool Comfirm { get; set; }
+    }
+}
